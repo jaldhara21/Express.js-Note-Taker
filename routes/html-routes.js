e342b57782a9d3640a8d68b3e9f8ec5f4d5ad61f
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-05 13:58:42 -0400

    Shift router in html_router.js file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,17 @@
 // Import the required modules
-const router = require('express').Router();
-const path = require('path');
+const router = require("express").Router();
+const path = require("path");
 
-// Send the index.html file located in the "public" directory
-router.get('/', (req, res) => {
-    // Send the index.html file located in the "public" directory
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 // Handle GET requests to the '/notes' route
 // Send the notes.html file located in the "public" directory
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// Send the index.html file located in the "public" directory
+router.get("*", (req, res) => {
+  // Send the index.html file located in the "public" directory
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // Export the router module to make it available for other files
