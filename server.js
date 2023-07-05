21180e12c9456f8cb8a0719742b85c7bd772aa5f
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-05 13:56:45 -0400

    Changed code in line 12,16 nd 17

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const html_routes = require('./routes/html-routes')
-const api_routes = require("./routes/api_routes")
+const html_routes = require("./routes/html-routes");
+const api_routes = require("./routes/api_routes");
 
 //Creating environment variable port
 const PORT = process.env.PORT || 3001;
@@ -9,15 +9,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Middleware for parsing JSON and urlencoded form data
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//Middleware to serve static files from the "public" directory 
-app.use (express.static("public"));
+//Middleware to serve static files from the "public" directory
+app.use(express.static("public"));
 //Middleware to handle HTML,API routes
-app.use(html_routes);
 app.use(api_routes);
+app.use(html_routes);
 
 //Start the server and listen on the specified port
 app.listen(PORT, () => {
-    console.log(`server running on http://localhost:${PORT}`);
+  console.log(`server running on http://localhost:${PORT}`);
 });
